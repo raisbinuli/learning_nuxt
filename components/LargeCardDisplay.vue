0283de684677ebc238346415a3a08df26ed6b110
RAIS27 <rais@gmd.id>
2021-03-20 16:56:45 +0700

    Create Large Card

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <h4 class="header">test</h4>
+        <p class="snippet">lorem ipsume</p>
+        <div class="cards-container">lorem ipsum</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.container {
+    margin-top: 2rem;
+}
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+    .snippet {
+        color: grey;
+        margin-bottom: 1.5rem;
+    }
+    .cards-container {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
